e8f540a792e467a7efbef93602385018a7016781
Vision <whostilluses@live.com>
2022-07-15 10:22:21 -0400

    Delete ClearTeamsCache.bat

--- ClearTeamsCache.bat
+++
@@ -1,8 +0,0 @@
-taskkill /im teams.exe /t /f
-del /f /q "%appdata%\Microsoft\teams\cache\*.*" > nul 2>&1
-del /f /q "%appdata%\Microsoft\teams\blob_storage\*.*" > nul 2>&1
-del /f /q "%appdata%\Microsoft\teams\databases\*.*" > nul 2>&1
-del /f /q "%appdata%\Microsoft\teams\GPUcache\*.*" > nul 2>&1
-del /f /q "%appdata%\Microsoft\teams\IndexdDB\*.db" > nul 2>&1
-del /f /q "%appdata%\Microsoft\teams\Local Storage\*.*" > nul 2>&1
-del /f /q "%appdata%\Microsoft\teams\tmp\*.*" > nul 2>&1
